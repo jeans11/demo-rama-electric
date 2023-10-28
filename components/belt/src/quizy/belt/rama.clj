c7bfb476cba1758e65f109f61d3fdefbc64f06a9
Jean BOUDET <jeanboudet@ip-192-168-8-176.eu-west-1.compute.internal>
2023-10-28 15:27:19 +0200

    refacto: use the proxy async function
    cf: https://clojurians.slack.com/archives/CL85MBPEF/p1698062851851949

--- components/belt/src/quizy/belt/rama.clj
+++ components/belt/src/quizy/belt/rama.clj
@@ -1,20 +1,17 @@
 (ns quizy.belt.rama
   (:require
    [com.rpl.rama :as r]
-   [missionary.core :as m])
-  (:import
-   (com.rpl.rama.diffs ResyncDiff)))
+   [missionary.core :as m]))
 
 (defn proxy-callback [f]
-  (fn [new diff _old]
-    (when (and new (not (instance? ResyncDiff diff)))
-      (f new))))
+  (fn [new _diff _old]
+    (f new)))
 
 (defn make-reactive-query [path pstate]
   (->> (m/observe
         (fn [!]
-          ;; Send the current value of the path as init value
-          (! (r/foreign-select-one path pstate))
-          (let [p (r/foreign-proxy path pstate {:callback-fn (proxy-callback !)})]
-            #(.close p))))
-       (m/latest (fn [i] i))))
+          ;; Init
+          (! nil)
+          (let [cp (r/foreign-proxy-async path pstate {:callback-fn (proxy-callback !)})]
+            #(.close @cp))))
+       (m/relieve {})))
