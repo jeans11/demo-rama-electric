1044640410b9422deee1883d6921a85dc1109558
Jean BOUDET <jeanboudet@ip-192-168-1-127.eu-west-1.compute.internal>
2023-10-31 13:43:29 +0100

    refacto: clean quiz/question parts

--- bases/web-ui/src/quizy/web_ui/views/board.cljc
+++ bases/web-ui/src/quizy/web_ui/views/board.cljc
@@ -46,6 +46,6 @@
               (dom/props {:class (styles :board/inner-container)})
               (case route-name
                 :quizzes (quiz-list/QuizList.)
-                :quiz (quiz-item/QuizItem. (uuid (-> route-match :path-params :id)))
                 :session (quiz-session/QuizSession. (uuid (-> route-match :path-params :id))
                                                     (uuid raw-user-id))))))))))
+                :quiz (quiz-item/QuizItem. (-> route-match :path-params :id))
--- components/quiz/src/quizy/quiz/core.clj
+++ components/quiz/src/quizy/quiz/core.clj
@@ -6,27 +6,6 @@
   (:import
    (java.util UUID)))
 
-(def quiz-data-schema
-  {UUID (r/fixed-keys-schema
-         {:title String
-          :description String
-          :level String
-          :max-player-per-session Long
-          :total-question Integer
-          :questions (r/vector-schema
-                      (r/fixed-keys-schema
-                       {:id UUID
-                        :max-second-to-answer Long
-                        :points Long}))})})
-
-(def question-data-schema
-  {UUID (r/fixed-keys-schema {:title String
-                              :right-answer UUID
-                              :choices (r/vector-schema (r/fixed-keys-schema {:id UUID
-                                                                              :value String}))})})
-(def quiz-sessions-schema
-  {UUID (r/vector-schema UUID)})
-
 (defrecord QuestionRecord [id title right-answer choices])
 (defrecord QuizRecord [id title level session-id description max-player-per-session questions])
 
@@ -34,8 +13,8 @@
 (def *quiz-depot "*quiz-depot")
 (def *question-depot "*question-depot")
 ;; pstates
-(def $$quiz "$$quiz")
-(def $$question "$$question")
+(def $$quizzes "$$quizzes")
+(def $$questions "$$questions")
 (def $$quiz-sessions "$$quiz-sessions")
 
 #_:clj-kondo/ignore
@@ -43,21 +22,37 @@
   (r/declare-depot setup *quiz-depot (r/hash-by :id))
   (r/declare-depot setup *question-depot (r/hash-by :id))
   (let [s (r/stream-topology topo "quiz")]
-    (r/declare-pstate s $$quiz quiz-data-schema)
-    (r/declare-pstate s $$question question-data-schema)
-    (r/declare-pstate s $$quiz-sessions quiz-sessions-schema)
+    ;; Quizzes schema
+    (r/declare-pstate s $$quizzes {String (r/fixed-keys-schema
+                                           {:title String
+                                            :description String
+                                            :level String
+                                            :max-player-per-session Long
+                                            :total-question Integer
+                                            :questions (r/vector-schema
+                                                        (r/fixed-keys-schema
+                                                         {:id String
+                                                          :max-second-to-answer Long
+                                                          :points Long}))})})
+    ;; Questions schema
+    (r/declare-pstate s $$questions {String (r/fixed-keys-schema
+                                             {:title String
+                                              :right-answer String
+                                              :choices (r/vector-schema
+                                                        (r/fixed-keys-schema
+                                                         {:id String
+                                                          :value String}))})})
+    ;; Quiz sessions schema
+    (r/declare-pstate s $$quiz-sessions {String (r/vector-schema String)})
+
     (r/<<sources s
                  ;; Question
-                 (r/source> *question-depot :> {:keys [*id *title *right-answer *choices]})
+                 (r/source> *question-depot :> {:keys [*id] :as *question})
                  (r/local-transform> [(path/keypath *id)
-                                      (path/multi-path [:title (path/termval *title)]
-                                                       [:right-answer (path/termval *right-answer)]
-                                                       [:choices (path/termval *choices)])]
-                                     $$question)
+                                      (path/termval (into {} (dissoc *question :id)))]
+                                     $$questions)
                  ;; Quiz
-                 (r/source> *quiz-depot :> {:keys [*id *title *description *level
-                                                   *max-player-per-session *session-id
-                                                   *questions]})
+                 (r/source> *quiz-depot :> {:keys [*id *session-id *questions] :as *quiz})
                  (count *questions :> *total-question)
                  (r/local-transform> [(path/keypath *id)
                                       path/NIL->LIST
@@ -65,13 +60,9 @@
                                       (path/termval *session-id)]
                                      $$quiz-sessions)
                  (r/local-transform> [(path/keypath *id)
-                                      (path/multi-path [:title (path/termval *title)]
-                                                       [:description (path/termval *description)]
-                                                       [:max-player-per-session (path/termval *max-player-per-session)]
-                                                       [:level (path/termval *level)]
-                                                       [:total-question (path/termval *total-question)]
-                                                       [:questions (path/termval *questions)])]
-                                     $$quiz))))
+                                      (path/termval (into {:total-question *total-question}
+                                                          (dissoc *quiz :id :session-id)))]
+                                     $$quizzes))))
 
 (def quiz-module-name (r/get-module-name QuizModule))
 
@@ -79,13 +70,13 @@
   (r/foreign-depot cluster quiz-module-name *question-depot))
 
 (defn get-questions-pstate [cluster]
-  (r/foreign-pstate cluster quiz-module-name $$question))
+  (r/foreign-pstate cluster quiz-module-name $$questions))
 
 (defn get-quiz-depot [cluster]
   (r/foreign-depot cluster quiz-module-name *quiz-depot))
 
 (defn get-quizzes-pstate [cluster]
-  (r/foreign-pstate cluster quiz-module-name $$quiz))
+  (r/foreign-pstate cluster quiz-module-name $$quizzes))
 
 (defn get-quiz-sessions-pstate [cluster]
   (r/foreign-pstate cluster quiz-module-name $$quiz-sessions))
@@ -95,24 +86,19 @@
     (r/foreign-append! question-depot question-record)))
 
 (defn send-quiz [depots quiz]
-  (let [session-id (random-uuid)
+  (let [session-id (str (random-uuid))
         quiz-record (map->QuizRecord (assoc quiz :session-id session-id))]
     (session/send-session (:session depots) {:id session-id
                                              :quiz-id (:id quiz-record)
                                              :users-id #{}})
-    (tap> quiz-record)
-    (tap> (type (:max-player-per-session quiz-record)))
     (r/foreign-append! (:quiz depots) quiz-record)))
 
-(defn get-quiz-module []
-  QuizModule)
+(defn get-quiz-module [] QuizModule)
 
 (defn get-quizzes [quizzes-pstate]
-  (let [all-quizzes (r/foreign-select path/ALL quizzes-pstate)]
-    (into []
-          (map (fn [[id quiz]]
-                 (assoc quiz :id id)))
-          all-quizzes)))
+  (into []
+        (map (fn [[k v]] (assoc v :id k)))
+        (r/foreign-select path/ALL quizzes-pstate)))
 
 (defn get-quiz-by-id [quizzes-pstate quiz-id]
   (r/foreign-select-one (path/keypath quiz-id) quizzes-pstate))
